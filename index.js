238ee4cb63d30edae1a4d9d43904085e7d8509f4
mpazzzvv <mariapazvv29@gmail.com>
2025-04-09 20:59:48 +0200

    added class on file

---
+++ index.js
@@ -0,0 +1,10 @@
+//Declarar una constante
+
+let count = 0;
+
+const countSpan = document.getElementById("count");
+countSpan.innerText = count;
+
+const button = document.getElementById("counter-button");
+
+button.addEventListener("click", () => {});
